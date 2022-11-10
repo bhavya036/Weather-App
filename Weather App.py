e778350433974a79eaaecab5d6459e59711c732d
Bhavya J Patel <85954191+bhavya036@users.noreply.github.com>
2022-11-10 16:18:37 +0530

    Add files via upload

---
+++ Weather App.py
@@ -0,0 +1,57 @@
+#LIVE WEATHER APP WITH TKINTER GUI
+
+from tkinter import *
+from tkinter import ttk #for combo box(For shawing Multiple option in a 1 click)
+import requests
+
+def data_get():
+    City = City_name.get()
+    data = requests.get("https://api.openweathermap.org/data/2.5/weather?q="+City+"&appid=3a01b4a65701defe543f80ea3f213f21").json()
+    w_label1.config(text = data["weather"][0]["main"])
+    wb_label1.config(text = data["weather"][0]["description"])
+    temp_label1.config(text = str(int(data["main"]["temp"]-273.15)))
+    per_label1.config(text = data["main"]["pressure"])
+
+    
+win = Tk()
+win.title("Tech Bjpatel")
+win.config(bg="pale green")
+win.geometry("500x500")
+
+name_label = Label(win,text="Accurate Weather App",fg = "darkblue",bg="lightpink",font=("Tmes New Roman",30,"bold"))
+name_label.place(x=25,y=50,height=50,width=450)
+
+City_name = StringVar()
+list_name = ["Andhra Pradesh","Arunachal Pradesh ","Assam","Bihar","Chhattisgarh",
+"Goa","Gujarat","Haryana","Himachal Pradesh","Jammu and Kashmir","Jharkhand","Karnataka","Kerala","Madhya Pradesh","Maharashtra",
+"Manipur","Meghalaya","Mizoram","Nagaland","Odisha","Punjab","Rajasthan","Sikkim","Tamil Nadu","Telangana","Tripura","Uttar Pradesh",
+"Uttarakhand","West Bengal","Andaman and Nicobar Islands","Chandigarh","Dadra and Nagar Haveli","Daman and Diu","Lakshadweep",
+"National Capital Territory of Delhi","Puducherry"]
+com = ttk.Combobox(win,font=("Times New Roman",15,"bold"),values = list_name,textvariable=City_name)
+com.place(x=25,y=120,height=50,width=450)
+
+w_label = Label(win,text="Weather Climate",fg = "darkblue",bg="lightpink",font=("Tmes New Roman",15))
+w_label.place(x=25,y=250,height=30,width=160)
+w_label1 = Label(win,text=" ",fg = "darkblue",bg="lightpink",font=("Tmes New Roman",15))
+w_label1.place(x=250,y=250,height=30,width=160)
+
+wb_label = Label(win,text="Weather Description",fg = "darkblue",bg="lightpink",font=("Tmes New Roman",13))
+wb_label.place(x=25,y=300,height=30,width=160)
+wb_label1 = Label(win,text=" ",fg = "darkblue",bg="lightpink",font=("Tmes New Roman",13))
+wb_label1.place(x=250,y=300,height=30,width=160)
+
+temp_label = Label(win,text="Temperature",fg = "darkblue",bg="lightpink",font=("Tmes New Roman",13))
+temp_label.place(x=25,y=350,height=30,width=160)
+temp_label1 = Label(win,text=" ",fg = "darkblue",bg="lightpink",font=("Tmes New Roman",13))
+temp_label1.place(x=250,y=350,height=30,width=160)
+
+per_label = Label(win,text="perssure",fg = "darkblue",bg="lightpink",font=("Tmes New Roman",13))
+per_label.place(x=25,y=400,height=30,width=160)
+per_label1 = Label(win,text=" ",fg = "darkblue",bg="lightpink",font=("Tmes New Roman",13))
+per_label1.place(x=250,y=400,height=30,width=160)
+
+
+done_button = Button(win,text="DONE",fg = "darkblue",bg="lightpink",font=("Tmes New Roman",20,"bold"),command = data_get())
+done_button.place(x=200,y=190,height=50,width=100)
+
+win.mainloop()
